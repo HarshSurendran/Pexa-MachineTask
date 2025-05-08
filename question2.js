6c64797a6c4373d0bef14b14014447e3d965e8fc
Harsh Surendran <harshsurendran@gmail.com>
2025-05-08 15:58:43 +0530

    Question number 2 completed.

---
+++ question2.js
@@ -0,0 +1,11 @@
+function debounce(func, waitTime) {
+    let timeout;
+    return function () {
+        const context = this;
+        const args = arguments;
+        clearTimeout(timeout);
+        timeout = setTimeout(() => {
+            func.apply(context, args);
+        }, waitTime);
+    };
+};
